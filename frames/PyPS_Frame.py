848377393c6bc50c0365126aeb2116f5c055ff27
Birnou75 <86726892+Birnou75@users.noreply.github.com>
2021-12-30 00:01:31 +0100

    Add files via upload

--- frames/PyPS_Frame.py
+++ frames/PyPS_Frame.py
@@ -5,6 +5,9 @@
 # --------------------------------------------------------------------
 
 from tkinter import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 pyps = Tk()
 
@@ -34,9 +37,33 @@ def changePaned(panedToDestroy, panedToPlace, buttonPanedName):
 def simulationSettings(paned, enable):
     if(paned == penduleSimulation):
         if(enable == True):
-            angle.place(relx=0.5, rely=0.25, relwidth=0.9, anchor=CENTER)
+            toolsScrollBar.configure(command=toolsButtons.yview)
+            toolsButtons.configure(yscrollcommand=toolsScrollBar.set)
+            toolsButtons.place(rely=0.12, relx=0.06, relwidth=0.82, relheight=0.8)
+            toolsScrollBar.pack(side=RIGHT, fill=Y)
+
+            """angle.pack(relx=0.05, rely=0.05, relwidth=0.9)
+            angle2.place(relx=0.05, rely=0.45, relwidth=0.9)
+            angle3.place(relx=0.05, rely=0.85, relwidth=0.9)
+            angle4.place(relx=0.05, rely=1.25, relwidth=0.9)"""
+            angle.pack(fill=X)
+            angle2.pack(fill=X)
+            angle3.pack(fill=X)
+            angle4.pack(fill=X)
+            
         else:
-            angle.place_forget()
+            toolsScrollBar.pack_forget()
+            toolsButtons.place_forget()
+            angle.pack_forget()
+            angle2.pack_forget()
+            angle3.pack_forget()
+            angle4.pack_forget()
+
+def penduleGraph():
+	
+
+	plt.show()
+	
 
 
 mainFont = "Roboto 12 bold"
@@ -64,10 +91,10 @@ homeButton = Button(header, text="Accueil", bg="#232369", fg="white", cursor="ha
 
 
 # Menu #
-menu = Frame(pyps, bg="white", highlightthickness=1)
+menu = Frame(pyps, bg="white", highlightthickness=2)
 menu.place(relx=0, rely=0.05, relwidth=0.3, relheight=0.9)
 
-tools = PanedWindow(menu, bg="#232369")
+tools = Frame(menu, bg="#232369")
 tools.place(relx=0.05, rely=0.05, relwidth=0.90, relheight=0.65)
 
 chooseTools = Label(tools, text="Choississez une simulation", bg="#232369", fg="white", font=mainFont)
@@ -76,7 +103,7 @@ chooseTools.place(relx=0.5, rely=0.5, anchor=CENTER)
 options = Button(menu, text="Options", bg="#232369", fg="white", cursor="hand2", font=mainFont)
 options.place(relx=0.05, rely=0.74, relwidth=0.90, relheight=0.10)
 
-creditsMenu = Button(menu, text="Histoire - Crédits", bg="#232369", fg="white", cursor="hand2", font=mainFont)
+creditsMenu = Button(menu, text="Histoire - Credits", bg="#232369", fg="white", cursor="hand2", font=mainFont)
 creditsMenu.place(relx=0.05, rely=0.87, relwidth=0.90, relheight=0.10)
 
 
@@ -103,7 +130,25 @@ soonButton.place(relx=0.55, rely=0.55, relwidth=0.35, relheight=0.35)
 # Panel de la simulation "Pendule"
 penduleSimulation = PanedWindow(main, bg="white")
 
-angle = Scale(tools, orient="horizontal", from_=0, to=360, resolution=0.1, tickinterval=90, label='Angle (deg)', bg="#232369", fg="white", font=mainFont)
+toolsButtons = Canvas(tools, bg="#232369")
+toolsScrollBar = Scrollbar(tools, orient=VERTICAL)
+
+angle = Scale(toolsButtons, orient="horizontal", from_=0, to=360, resolution=1, tickinterval=180, label='Angle (deg)', bg="#232369", fg="white", font=mainFont)
+angle2 = Scale(toolsButtons, orient="horizontal", from_=0, to=360, resolution=1, tickinterval=180, label='Angle (deg)', bg="#232369", fg="white", font=mainFont)
+angle3 = Scale(toolsButtons, orient="horizontal", from_=0, to=360, resolution=1, tickinterval=180, label='Angle (deg)', bg="#232369", fg="white", font=mainFont)
+angle4 = Scale(toolsButtons, orient="horizontal", from_=0, to=360, resolution=1, tickinterval=180, label='Angle (deg)', bg="#232369", fg="white", font=mainFont)
+angle5 = Scale(toolsButtons, orient="horizontal", from_=0, to=360, resolution=1, tickinterval=180, label='Angle (deg)', bg="#232369", fg="white", font=mainFont)
+
+infosPendule = Label(penduleSimulation, text="Vitesse : 0 m/s \n Temps restant : 0h, 0m, 0s", bg="#232369", fg="white", font=mainFont)
+infosPendule.place(relx=0.6, rely=0.9, relwidth=0.4, relheight=0.09)
+
+	# Animation, partie majeure
+animationPendule = Canvas(penduleSimulation, bg="#33fff3")
+animationPendule.place(relx=0.025, rely=0.05, relheight=0.8, relwidth=0.95)
+
+graphButton = Button(animationPendule, text="Lancer animation\n Version Test", bg="#232369", fg="white", cursor="hand2", command=lambda:penduleGraph(), font=mainFont)
+graphButton.place(relx=0.5, rely=0.5, anchor=CENTER)
+
 
 # Panel de la simulation "Pendule de Newton"
 penduleNSimulation = PanedWindow(main, bg="white")
@@ -118,6 +163,11 @@ footer.place(relx=0, rely=0.95, relwidth=1, relheight=0.05)
 
 footerCredits = Label(footer, text="Version 1.0 | Par Nizar & Loic", bg="#232369", fg="white", font=mainFont)
 footerCredits.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+
+# Logo #
+
+
 
 
 # --------------------------------------------------------------------
