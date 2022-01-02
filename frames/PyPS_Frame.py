30e462e9dd5bae5b00a028991b54ba1d17abc741
Birnou75 <86726892+Birnou75@users.noreply.github.com>
2022-01-02 16:52:25 +0100

    Add files via upload
    (+) Pendulum animation with matplotlib (animation matplotlib is embedded in the main frame)
    (+) Pendulum settings, start animation button, pendulum informations

--- frames/PyPS_Frame.py
+++ frames/PyPS_Frame.py
@@ -5,9 +5,10 @@
 # --------------------------------------------------------------------
 
 from tkinter import *
-import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from math import *
 
 pyps = Tk()
 
@@ -42,10 +43,6 @@ def simulationSettings(paned, enable):
             toolsButtons.place(rely=0.12, relx=0.06, relwidth=0.82, relheight=0.8)
             toolsScrollBar.pack(side=RIGHT, fill=Y)
 
-            """angle.pack(relx=0.05, rely=0.05, relwidth=0.9)
-            angle2.place(relx=0.05, rely=0.45, relwidth=0.9)
-            angle3.place(relx=0.05, rely=0.85, relwidth=0.9)
-            angle4.place(relx=0.05, rely=1.25, relwidth=0.9)"""
             angle.pack(fill=X)
             angle2.pack(fill=X)
             angle3.pack(fill=X)
@@ -59,12 +56,55 @@ def simulationSettings(paned, enable):
             angle3.pack_forget()
             angle4.pack_forget()
 
-def penduleGraph():
+# thetaMax en radian, r en mètre, g en m/s², t en seconde
+def updatePenduleAnimation(thetaMaxUpdate, rUpdate, gUpdate, thetaUpdate):
+	pendulePlot.clear()
 	
+	ax.grid(True)
+	ax.spines['left'].set_position('zero')
+	ax.spines['bottom'].set_position('zero')
+	pendulePlot.set_xlim([-pi, pi])
+	pendulePlot.set_ylim([-pi, pi])
 
-	plt.show()
+	pendulePlot.scatter([0, rUpdate*cos(thetaUpdate-pi/2)], [0, rUpdate*sin(thetaUpdate-pi/2)])
+	pendulePlot.plot([0, rUpdate*cos(thetaUpdate-pi/2)], [0, rUpdate*sin(thetaUpdate-pi/2)])
 	
+	animationCanvas.draw()
+	
+def penduleAnimation(g, t):
+	periode = 2*pi*sqrt(r/g)
+	infosPendule['text'] = "Période : {} s\n Temps restant : 0h, 0m, 0s".format(round(periode, 1))
+	
+	h = 0.1
+	omega = sqrt(g/r)
+	
+	for i in range(0, int(t/h)):
+		thetaAnim = (thetaMax)*sin(omega*i*h + pi/2)
+		updatePenduleAnimation(thetaMax, r, g, thetaAnim)
+		plt.pause(h/2.6)
+	
+def changeR(newR):
+	global r
+	r = float(newR)
+	updatePenduleAnimation(thetaMax, r, g, theta)
+	
+def changeThetaMax(newThetaMax):
+	global thetaMax
+	global theta
+	thetaMax = (float(newThetaMax)*pi)/180
+	theta = (thetaMax)*sin(pi/2)
+	updatePenduleAnimation(thetaMax, r, g, theta)
+	
+def changeG(newG):
+	global g
+	g = float(newG)
+	
+def changeT(newT):
+	global t
+	t = float(newT)
 
+    
+	
 
 mainFont = "Roboto 12 bold"
 
@@ -133,21 +173,40 @@ penduleSimulation = PanedWindow(main, bg="white")
 toolsButtons = Canvas(tools, bg="#232369")
 toolsScrollBar = Scrollbar(tools, orient=VERTICAL)
 
-angle = Scale(toolsButtons, orient="horizontal", from_=0, to=360, resolution=1, tickinterval=180, label='Angle (deg)', bg="#232369", fg="white", font=mainFont)
-angle2 = Scale(toolsButtons, orient="horizontal", from_=0, to=360, resolution=1, tickinterval=180, label='Angle (deg)', bg="#232369", fg="white", font=mainFont)
-angle3 = Scale(toolsButtons, orient="horizontal", from_=0, to=360, resolution=1, tickinterval=180, label='Angle (deg)', bg="#232369", fg="white", font=mainFont)
-angle4 = Scale(toolsButtons, orient="horizontal", from_=0, to=360, resolution=1, tickinterval=180, label='Angle (deg)', bg="#232369", fg="white", font=mainFont)
-angle5 = Scale(toolsButtons, orient="horizontal", from_=0, to=360, resolution=1, tickinterval=180, label='Angle (deg)', bg="#232369", fg="white", font=mainFont)
+angle = Scale(toolsButtons, orient="horizontal", from_=0, to=180, resolution=1, tickinterval=90, label='Angle (deg)', bg="#232369", fg="white", command=changeThetaMax, font=mainFont)
+angle2 = Scale(toolsButtons, orient="horizontal", from_=1, to=3, resolution=0.01, tickinterval=1, label='Longueur du fil (mètres)', bg="#232369", fg="white", command=changeR, font=mainFont)
+angle3 = Scale(toolsButtons, orient="horizontal", from_=1, to=10, resolution=1, tickinterval=2, label='Gravité (lieu)', bg="#232369", fg="white", command=changeG, font=mainFont)
+angle4 = Scale(toolsButtons, orient="horizontal", from_=1, to=100, resolution=1, tickinterval=30, label='Temps de la simulation', bg="#232369", fg="white", command=changeT, font=mainFont)
 
-infosPendule = Label(penduleSimulation, text="Vitesse : 0 m/s \n Temps restant : 0h, 0m, 0s", bg="#232369", fg="white", font=mainFont)
+infosPendule = Label(penduleSimulation, text="Période : 0 s\n Temps restant : 0h, 0m, 0s", bg="#232369", fg="white", font=mainFont)
 infosPendule.place(relx=0.6, rely=0.9, relwidth=0.4, relheight=0.09)
 
 	# Animation, partie majeure
-animationPendule = Canvas(penduleSimulation, bg="#33fff3")
+animationPendule = Canvas(penduleSimulation, bg="#ff4040")
 animationPendule.place(relx=0.025, rely=0.05, relheight=0.8, relwidth=0.95)
 
-graphButton = Button(animationPendule, text="Lancer animation\n Version Test", bg="#232369", fg="white", cursor="hand2", command=lambda:penduleGraph(), font=mainFont)
-graphButton.place(relx=0.5, rely=0.5, anchor=CENTER)
+graphButton = Button(penduleSimulation, text="Lacher le pendule", bg="#ff4040", fg="white", cursor="hand2", command=lambda:penduleAnimation(g, t), font=mainFont)
+graphButton.place(relx=0.025, rely=0.92, relwidth=0.3, relheight=0.07)
+
+penduleFig = Figure(figsize=(4, 4), dpi=100)
+pendulePlot = penduleFig.add_subplot(111)
+
+ax = penduleFig.gca()
+ax.grid(True)
+ax.spines['left'].set_position('zero')
+ax.spines['bottom'].set_position('zero')
+pendulePlot.set_xlim([-pi, pi])
+pendulePlot.set_ylim([-pi, pi])
+
+(thetaMax, r, g, t) = (0, 1, 1, 10)
+theta = (thetaMax)*sin(thetaMax)
+
+pendulePlot.scatter([0, r*cos(theta-pi/2)], [0, r*sin(theta-pi/2)])
+pendulePlot.plot([0, r*cos(theta-pi/2)], [0, r*sin(theta-pi/2)])
+
+animationCanvas = FigureCanvasTkAgg(penduleFig, master = animationPendule)
+animationCanvas.draw()
+animationCanvas.get_tk_widget().place(relx=0.5, rely=0.5, anchor=CENTER)
 
 
 # Panel de la simulation "Pendule de Newton"
@@ -161,7 +220,7 @@ bouleSimulation = PanedWindow(main, bg="white")
 footer = Frame(pyps, bg="#232369")
 footer.place(relx=0, rely=0.95, relwidth=1, relheight=0.05)
 
-footerCredits = Label(footer, text="Version 1.0 | Par Nizar & Loic", bg="#232369", fg="white", font=mainFont)
+footerCredits = Label(footer, text="Version 1.0 | Par Nizar", bg="#232369", fg="white", font=mainFont)
 footerCredits.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 
